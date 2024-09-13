965e21482f4ea3a6c206c7aa7c85e813f030094d
rakagawa17 <chichuanliaotailang@gmail.com>
2024-09-13 19:40:30 +0900

    totalsegmentatorのscriptの修正

--- totalSegmentator/script/dcm2nifti.py
+++ totalSegmentator/script/dcm2nifti.py
@@ -10,9 +10,9 @@ def convert_dicom_to_nifti(dicom_folder, nifti_output_folder):
     for case in os.listdir(dicom_folder):
         case_path = os.path.join(dicom_folder, case)
         if os.path.isdir(case_path):
-            # CT1フォルダを探す
-            ct1_path = os.path.join(case_path, 'CT1')
-            # ct1_path = os.path.join(case_path, 'CT2')
+            # CT1 or CT2フォルダを探す
+#            ct1_path = os.path.join(case_path, 'CT1')
+             ct2_path = os.path.join(case_path, 'CT2')
             if os.path.isdir(ct1_path):
                 # 一時出力先ディレクトリを指定
                 temp_output_folder = os.path.join(nifti_output_folder, "temp")
@@ -26,19 +26,19 @@ def convert_dicom_to_nifti(dicom_folder, nifti_output_folder):
                     for file_name in os.listdir(temp_output_folder):
                         if file_name.endswith('.nii.gz'):
                             source_file = os.path.join(temp_output_folder, file_name)
-                            target_file = os.path.join(nifti_output_folder, f"{case}_CT1.nii.gz")
+                            target_file = os.path.join(nifti_output_folder, f"{case}_CT2.nii.gz")
                             # target_file = os.path.join(nifti_output_folder, f"{case}_CT2.nii.gz")
                             shutil.move(source_file, target_file)
-                            print(f"Successfully converted {ct1_path} to {target_file}")
+                            print(f"Successfully converted {ct2_path} to {target_file}")
                 except Exception as e:
-                    print(f"Failed to convert {ct1_path}: {str(e)}")
+                    print(f"Failed to convert {ct2_path}: {str(e)}")
                 finally:
                     # 一時出力先ディレクトリをクリーンアップ
                     shutil.rmtree(temp_output_folder)
 
 # 使用例
-dicom_folder = ''  # DICOMファイルの親フォルダへのパス
-nifti_output_folder = ''  # NIfTIファイルを保存するフォルダへのパス
+dicom_folder = '/dcmfolder'  # DICOMファイルの親フォルダへのパス
+nifti_output_folder = '/niftifolder'  # NIfTIファイルを保存するフォルダへのパス
 
 convert_dicom_to_nifti(dicom_folder, nifti_output_folder)
 
--- totalSegmentator/script/totalsegmentator.py
+++ totalSegmentator/script/totalsegmentator.py
@@ -3,7 +3,8 @@ import nibabel as nib
 import numpy as np
 import os
 
-def process_file(input_file, output_folder):
+#スライス分割のためthyroid_gland, whole_lung, kidneyの3つのマスクを作成する
+def thyroid_gland_segmentation(input_file, output_folder):
     # 患者IDをファイル名から取得
     patient_id = os.path.basename(input_file)[:-7]  # '.nii.gz'を除外
     output_path = os.path.join(output_folder, patient_id)
@@ -45,16 +46,22 @@ def process_file(input_file, output_folder):
     os.remove(thyroid_path)
     os.remove(ref_img_path)
 
+# def whole_lung_segmentation(input_file, output_folder):
+
+# def kidney_segmentation(input_file, output_folder):
+
 def main():
     # 入力フォルダと出力フォルダのパスを指定
-    input_folder = ''
-    output_folder = ''
+    input_folder = '/niftifolder'
+    output_folder = 'organSeg/dataset'
 
     # 入力フォルダ内の全ての .nii.gz ファイルを処理
     for filename in os.listdir(input_folder):
         if filename.endswith('.nii.gz'):
             input_file = os.path.join(input_folder, filename)
-            process_file(input_file, output_folder)
+            thyroid_gland_segmentation(input_file, output_folder)
+            # whole_lung_segmentation(input_file, output_folder)
+            # kidney_segmentation(input_file, output_folder)
 
 if __name__ == "__main__":
     from multiprocessing import freeze_support
